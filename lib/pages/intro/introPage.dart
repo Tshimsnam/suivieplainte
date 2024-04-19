794f93bc7081b3e5e213c6c4629dd27342d6a38e
Tshimsnam <manassetshims@gmail.com>
2024-04-19 16:47:45 +0100

    mise a jours

--- lib/pages/intro/introPage.dart
+++ lib/pages/intro/introPage.dart
@@ -4,70 +4,105 @@ class IntroPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Padding(padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),),
-          SizedBox(width: double.infinity,),
-          SizedBox(height: 40,),
+      body: Padding(
 
-          Text("Créer un Agent "),
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),),
+            SizedBox(width: double.infinity,),
+            SizedBox(height: 40,),
 
-          SizedBox(height: 20,),
+            Text(
+              'Créer un Agent',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
 
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Nom Agent',
-    labelStyle: TextStyle(
-    color: Colors.blue, // Couleur du texte du label
-    fontWeight: FontWeight.bold, // Gras
-    fontSize: 16.0, // Taille de police
-    ),
-    focusedBorder: UnderlineInputBorder(
-    borderSide: BorderSide(color: Colors.blue,  width: 2.0,), // Couleur de la ligne lorsque le champ est sélectionné
-    ),
-    ),
-    ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Prenom Agent',
-            ),
-          ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Sexe Agent',
-            ),
-          ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'adresse_mail Agent',
-            ),
-          ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Guichet Agent',
-            ),
-          ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Post Agent',
-            ),
-          ),
-          SizedBox(height: 20,),
-          SizedBox(width: 200, height:60, child: ElevatedButton(onPressed:(){}, child: Text("Enregistrer")))
+            SizedBox(height: 20,),
 
-        ],
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Nom Agent',
+            labelStyle: TextStyle(
+            color: Colors.black54, // Couleur du texte du label
+            fontWeight: FontWeight.bold, // Gras
+            fontSize: 16.0, // Taille de police
+            ),
+            focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue,  width: 2.0,), // Couleur de la ligne lorsque le champ est sélectionné
+            ),
+            ),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Prenom Agent',
+                labelStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight:  FontWeight.bold,
+                  fontSize: 16.0,
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Sexe Agent',
+                labelStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight:  FontWeight.bold,
+                  fontSize: 16.0,
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'adresse_mail Agent',
+                labelStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Guichet Agent',
+                labelStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Post Agent',
+                labelStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            SizedBox(width: 200, height:60, child: ElevatedButton(onPressed:(){}, child: Text("Enregistrer")))
+
+          ],
+        ),
       ),
     );
   }
